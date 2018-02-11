1786109d8db7a9773980d949dff51ba5ba73d6bb
Ros Haitovich <matrostik@gmail.com>
2018-02-11 22:40:16 +0200

    jquery + postmonger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import * as Postmonger from 'postmonger';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    console.log('*** ' + window.location.href  + ' ***');
+    const connection = new Postmonger.Session();
+
+    $(window).ready(function() {
+      console.log('ready + request Endpoints');
+      connection.trigger('ready');
+
+      // TODO just for testing
+      connection.trigger('requestEndpoints');
+      connection.trigger('requestTokens');
+  });
+
+    $(document).ready(function() {
+      $('#button').click(function() {
+        console.log('sdfsdd');
+          // var div = $('div');
+          // div.animate({left: '100px'}, 'slow');
+          // div.animate({fontSize: '5em'}, 'slow');
+      });
+  });
+  }
 }
